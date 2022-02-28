a9ffd8bac4370be51bca2eb4dce884af565a44af
Yuri Golikov <meetmatt@ya.ru>
2022-02-28 15:25:01 +0100

    Chore

--- test/suite/integration/Specification/FactoryTest.php
+++ test/suite/integration/Specification/FactoryTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MeetMatt\OpenApiSpecCoverage\Test\Suite\Integration\Specification;
 
 use Codeception\Test\Unit;
--- test/support/IntegrationTester.php
+++ test/support/IntegrationTester.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MeetMatt\OpenApiSpecCoverage\Test\Support;
 
 use Codeception\Actor;
